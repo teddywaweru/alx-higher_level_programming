99409da7fc32aacf7240679320971cd1f33c2c42
teddywaweru <teddywaweru@gmail.com>
2023-01-11 00:30:15 +0300

    add: solutions for 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,22 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     (a, b) = (0, 0)
-    tuple_c = ()
+    list_c = []
     for idx in range(0, 2):
         if not tuple_a[idx]:
             if not tuple_b[idx]:
-                tuple_c = tuple_c + (a + b)
-            b = tuple_b[idx]
+                list_c.append(0)
+            else:
+                b = tuple_b[idx]
+                list_c.append(0 + b)
+        else:
+            a = tuple_a[idx]
+            if not tuple_b[idx]:
+                list_c.append(0 + a)
+            else:
+                b = tuple_b[idx]
+                list_c.append(a + b)
 
-            tuple_c += a + b
+    tuple_c = tuple(list_c)
+
+    return tuple_c
